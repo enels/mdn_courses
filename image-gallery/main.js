00195ed7b740b38199672669cbbc4c5d42440926
Enoma Uwaifo <enomauwaifo@gmail.com>
2025-03-10 18:05:13 +0100

    loop over the images displaying them as thumbnails

--- image-gallery/main.js
+++ image-gallery/main.js
@@ -5,14 +5,25 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const imgArray = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
+const altObj = ['Closeup of a human eye', 'bed fold', 'hibiscus flowers', 'ancient egypt', 'butterfly on flower'];
 
 /* Looping through images */
+for (let i = 0; i < imgArray.length; i++) {
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+    // create the img element
+    const newImage = document.createElement('img');
+
+    // set the image source
+    newImage.setAttribute('src', `images/${imgArray[i]}`);
+
+    // set the image alt text
+    newImage.setAttribute('alt', altObj[i]);
+
+    thumbBar.appendChild(newImage);
+    
+}
 
 /* Wiring up the Darken/Lighten button */
